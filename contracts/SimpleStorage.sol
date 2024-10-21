877ae89e48b5926b3f76519b928df65e90388f4b
JosephEdando <josephyedando25@gmail.com>
2024-10-20 20:41:46 -0700

    My First Smart Contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,16 +2,5 @@
 pragma solidity ^0.8.25;
 
 contract SimpleStorage {
-
-  uint256  favorite_Number;
-
-  function storeNumber(uint256 _favorite_Number)   public {
-      
-    favorite_Number = _favorite_Number;
-  }
-
-  function get_Favorite_Number() public view returns (uint256) {
-      return favorite_Number;
-  }
-
+    uint256 public favoriteNumber; // State variable to store a number
 }
